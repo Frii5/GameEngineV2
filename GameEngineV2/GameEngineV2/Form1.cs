0f861750a4ff4670f9afe5f5635c0e02943bce2a
Frii5 <Kristian.friis@live.dk>
2017-11-17 11:06:30 +0100

    Commit my man

---
+++ GameEngineV2/GameEngineV2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameEngineV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //awkdlakwjdlawjdlkawjdla i wonr ro die in a hole of pitiful despair. please extrude my soul from my body and implant hells imbrace into my wretched sinnister being
+        }
+    }
+}
